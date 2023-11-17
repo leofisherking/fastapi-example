7758c714a7d9bd4e5bd69d13e69ea2bc30a7e3c4
Lev <l.mikheesku@remont.io>
2023-11-17 18:45:57 +0300

    schemas validation fix

--- src/auth/schemas.py
+++ src/auth/schemas.py
@@ -1,9 +1,10 @@
 import uuid
 from fastapi_users import schemas
-
+from typing import Annotated
+from annotated_types import MinLen, MaxLen
 
 class UserRead(schemas.BaseUser[uuid.UUID]):
-    username: str
+    username: Annotated[str, MinLen(3), MaxLen(16)]
 
 class UserCreate(schemas.BaseUserCreate):
-    username: str
+    username: Annotated[str, MinLen(3), MaxLen(16)]
--- src/entity/schemas.py
+++ src/entity/schemas.py
@@ -1,13 +1,15 @@
-from pydantic import BaseModel, constr, PositiveFloat
+from typing import Annotated
+from annotated_types import MinLen, MaxLen
+from pydantic import BaseModel, PositiveFloat
 
 
 class Tag(BaseModel):
-    name: constr(max_length=16, min_length=3)
+    name: Annotated[str, MinLen(3), MaxLen(16)]
 
 
 class Entity(BaseModel):
-    name: constr(max_length=16, min_length=3)
-    description: constr(max_length=1024) | None
+    name: Annotated[str, MinLen(3), MaxLen(16)]
+    description: Annotated[str, MaxLen(1024)] | None
     price: PositiveFloat
 
     #tags: list[Tag] | None
